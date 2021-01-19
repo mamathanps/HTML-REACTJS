351a052f772874dffc839a85f962f7c385ac3318
mamathanps <mamatha.k8050@gmail.com>
2021-01-20 02:58:41 +0530

    Link to Router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,35 @@ import React from "react";
 import logo from './logo.svg';
 import './App.css';
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Service from "./pages/Service";
+import Single from "./pages/Single";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
+      <Router>
+
         <Header />
-        <Home />
+
+          <Switch>
+             <Route exact path="/" component={Home} />
+             <Route path="/About" component={About} />
+             <Route path="/service" component={Service} />
+             <Route path="/single" component={Single} />
+             <Route path="/gallery" component={Gallery} />
+             <Route path="/contact" component={Contact} />
+          </Switch>
+
         <Footer />
+
+      </Router>
     </div>
     
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 
 class Header extends React.Component {
@@ -11,9 +12,9 @@ class Header extends React.Component {
 		<div class="header d-lg-flex justify-content-between align-items-center">
 			<div class="header-agile">
 				<h1>
-					<a class="navbar-brand logo" href="index.html">
-						<span class="fa fa-scissors" aria-hidden="true" ></span> Men spa	<span class="fa fa-user-secret" aria-hidden="true" ></span>
-					</a>
+					<Link to="/" class="navbar-brand logo">
+						<span class="fa fa-scissors" aria-hidden="true" ></span> Barbie Girls	<span class="fa fa-user-secret" aria-hidden="true" ></span>
+				    </Link>
 				</h1>
 			</div>
 			<div class="nav_w3ls">
@@ -21,20 +22,20 @@ class Header extends React.Component {
 					<label for="drop" class="toggle mt-lg-0 mt-1"><span class="fa fa-bars" aria-hidden="true"></span></label>
 					<input type="checkbox" id="drop" />
 						<ul class="menu">
-							<li class="mr-lg-3 mr-2 active"><a href="index.html">Home</a></li>
-							<li class="mr-lg-3 mr-2"><a href="about.html">About </a></li>
+							<li class="mr-lg-3 mr-2 active"><Link to="/">Home</Link></li>
+							<li class="mr-lg-3 mr-2"><Link to="/About">About</Link></li>
 							<li class="mr-lg-3 mr-2 p-0">
 							{/* <!-- First Tier Drop Down --> */}
 							<label for="drop-2" class="toggle">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span> </label>
 							<a href="#">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span></a>
 							<input type="checkbox" id="drop-2"/>
 							<ul class="inner-dropdown">
-								<li><a href="services.html">What We Do</a></li>
-								<li><a href="single.html">See more</a></li>
+								<li><Link to="/Service">Our Services</Link></li>
+								<li><Link to="/Single">Signle Page</Link></li>
 							</ul>
 							</li>
-							<li class="mr-lg-3 mr-2"><a href="gallery.html">Gallery</a></li>
-							<li class="mr-lg-3 mr-2"><a href="contact.html">Contact Us</a></li>
+							<li class="mr-lg-3 mr-2"><Link to="/Gallery">Gallery</Link></li>
+							<li class="mr-lg-3 mr-2"><Link to="/Contact">Contact US</Link></li>
 							
 
 						</ul>
